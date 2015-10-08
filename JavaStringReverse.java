9b228f2d81307c167a1deb3e75b513db095556bc
Howard Canaway <zehaeva@users.noreply.github.com>
2015-10-08 17:36:24 -0400

    Create JavaStringReverse.java

---
+++ JavaStringReverse.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        StringBuilder B =  new StringBuilder(A.trim());
+        StringBuilder C =  new StringBuilder(A.trim());
+        if (C.toString().equals( B.reverse().toString())) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
